bf3f49a835c1d058634b37b626f56aacc912b90b
Jared Costenbader <costenbaderjared@gmail.com>
2024-12-13 11:34:10 -0500

    Created 2.1

---
+++ AdventCalender2024/AdventDay2/Advent2.1.cs
@@ -0,0 +1,11 @@
+﻿// Read the file line by line
+
+Boolean Safe = true;
+
+string[] lines = File.ReadAllLines("TextFile2.1.txt");
+List<int> Levels = new List<int>();
+
+foreach (string line in lines)
+{
+
+}
