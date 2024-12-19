1fb0cb7141db2d42d74decd23b46cecc52f535c0
Jared Costenbader <costenbaderjared@gmail.com>
2024-12-19 11:33:14 -0500

    Attempted 2.1
    Currently only results in the number of safe always being 0

--- AdventCalender2024/AdventDay2/Advent2.1.cs
+++ AdventCalender2024/AdventDay2/Advent2.1.cs
@@ -1,11 +1,62 @@
-﻿// Read the file line by line
-
-Boolean Safe = true;
-
-string[] lines = File.ReadAllLines("TextFile2.1.txt");
-List<int> Levels = new List<int>();
-
-foreach (string line in lines)
+﻿class Program
 {
+    static void Main()
+    {
+        int SafeReportNum = 0;
 
+        string[] lines = File.ReadAllLines("TextFile2.1.txt");
+
+        foreach (string line in lines)
+        {
+            // Converts the line to a list of integers (levels)
+            List<int> Levels = line.Split(' ').Select(int.Parse).ToList();
+
+            if (IsSafeReport(Levels))
+            {
+                SafeReportNum++;
+            }
+        }
+
+        Console.WriteLine("Number of safe reports: " + SafeReportNum);
+    }
+
+    static bool IsSafeReport(List<int> levels)
+    {
+        int n = levels.Count;
+
+        if (n < 2)
+        {
+            return false;
+        }
+
+        bool ascending = true;
+        bool descending = true;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int difference = levels[i + 1] - levels[i];
+
+            if (difference < 1 || difference > 3)
+            {
+                return false;
+            }
+
+            if (levels[i] < levels[i + 1])
+            {
+                descending = false;
+            }
+            else if (levels[i] > levels[i + 1])
+            {
+                ascending = false;
+            }
+
+            // If both flags are false, break early to avoid unnecessary iterations
+            if (!ascending && !descending)
+            {
+                return false;
+            }
+        }
+
+        return ascending || descending;
+    }
 }
